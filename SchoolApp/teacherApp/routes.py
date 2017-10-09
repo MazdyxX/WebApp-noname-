baff5c78d450bf21960594b4d696450eb3b4f234
MaksymilianDemitraszek <maksymilian.demitraszek@gmail.com>
2017-10-09 17:30:10 +0200

    nopp

--- SchoolApp/teacherApp/routes.py
+++ SchoolApp/teacherApp/routes.py
@@ -1,8 +1,22 @@
 from flask import Blueprint, render_template, request
 
 
-teacherpage = Blueprint('teacherpage', __name__, template_folder='templates', static_folder='static/teacherpage')
+teacherpage = Blueprint('teacherpage', __name__, template_folder='templates', static_folder='static')
 
+#TESTSTUFF!!DELETE#
+students = ['Tomek Marek','Adam Stańczuk','Piotr Karolak']
+classes = ['2F', '3B', '4C', '4A', '5M']
 @teacherpage.route('/')
 def index():
-    return render_template('teacher.html')
+    content = {
+        'classes': classes
+    }
+    return render_template('teacher.html', content=content)
+@teacherpage.route('/<class_id>')
+def class_list(class_id):
+    #here will be query for students in class#
+    content = {
+        'classes': classes,
+        'students_ofclass': students
+    }
+    return render_template('classlist.html', content = content)
