ce7b06326548367dd077f2c818e7509d3b02d294
MaksymilianDemitraszek <32527780+MaksymilianDemitraszek@users.noreply.github.com>
2017-10-10 08:06:55 +0200

    Update work.py

--- work.py
+++ work.py
@@ -1,1 +1,8 @@
+from flask import Blueprint, render_template, request
 
+
+adminpage = Blueprint('teacherpage', __name__, template_folder='templates', static_folder='static')
+
+@adminpage.route('')
+def index():
+  
