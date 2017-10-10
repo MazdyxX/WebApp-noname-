90b066589fe81b94f2634d307055a362a05fee89
MaksymilianDemitraszek <32527780+MaksymilianDemitraszek@users.noreply.github.com>
2017-10-10 08:38:15 +0200

    Update classswap.js

--- SchoolApp/teacherApp/static/classswap.js
+++ SchoolApp/teacherApp/static/classswap.js
@@ -7,12 +7,3 @@ $.ajax({
 });
 }
 
-function giveForm(id){
-$.ajax({
-  type: 'GET',
-  url: '/teacher/edit/' + id,
-  context: document.body
-}).done(function(response) {
-  $('#class_members').html(response);
-});
-}
