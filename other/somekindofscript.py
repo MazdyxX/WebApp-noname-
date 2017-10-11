504a5bb0d89488478e689698390be9a5b9e03a52
MaksymilianDemitraszek <maksymilian.demitraszek@gmail.com>
2017-10-12 00:02:51 +0200

    r

--- other/somekindofscript.py
+++ other/somekindofscript.py
@@ -11,4 +11,6 @@ def get_account_info():
 
 get_account_info()
 
-
+#[{"teacher_name":"Tomek Marek","key_code":"MxA49CxyJu","key_pass":"A1eBB"},
+# {"teacher_name":"Adam Staczuk","key_code":"mnumWrqSDZ","key_pass":"H3zCP"},{"teacher_name":"Piotr Karolak","key_code":"gQDkWAYtVw","key_pass":"eN3SO"}]
+#testmichnoV10
